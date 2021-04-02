936df2d477935528ee47cf4569318abd945b5116
Alex Mazansky <alex@amazansky.com>
2021-04-02 13:15:06 -0400

    edits to flag generator for hawkhacks ctf

--- genflag.py
+++ genflag.py
@@ -4,40 +4,42 @@
 import argparse
 import random
 import math
+import secrets
+import string
 
 char_map = [
-        ['A', 'a', '4', '@'],
-        ['B', 'b', '8'],
-        ['C', 'c'],
-        ['D', 'd'],
-        ['E', 'e', '3'],
-	['F', 'f'],
-	['G', 'g', '6', '9'],
-	['H', 'h'],
-	['I', 'i', '1'],
-	['J', 'j'],
-	['K', 'k'],
-	['L', 'l', '1'],
-	['M', 'm'],
-	['N', 'n'],
-	['O', 'o', '0'],
-	['P', 'p'],
-	['Q', 'q'],
-	['R', 'r'],
-	['S', 's', '5', '$'],
-	['T', 't', '7'],
-	['U', 'u'],
-	['V', 'v'],
-	['W', 'w'],
-	['X', 'x'],
-	['Y', 'y'],
-	['Z', 'z', '2'],
-        ]
+        ['a', '4', '@'],
+        ['b', '8'],
+        ['c'],
+        ['d'],
+        ['e', '3'],
+        ['f'],
+        ['g', '6', '9'],
+        ['h'],
+        ['i', '1'],
+        ['j'],
+        ['k'],
+        ['l', '1'],
+        ['m'],
+        ['n'],
+        ['o', '0'],
+        ['p'],
+        ['q'],
+        ['r'],
+        ['s', '5', '$'],
+        ['t', '7'],
+        ['u'],
+        ['v'],
+        ['w'],
+        ['x'],
+        ['y'],
+        ['z', '2'],
+    ]
 
 def change(c):
     if c.isalpha():
-        c = c.upper()
-        char_set = char_map[ord(c) - ord('A')]
+        c = c.lower()
+        char_set = char_map[ord(c) - ord('a')]
         new_c = char_set[random.randint(0, len(char_set) - 1)]
         return new_c, (c == new_c) * math.log2(len(char_set))
     else:
@@ -45,15 +47,17 @@ def change(c):
 
 def main():
     global args
+    chars = string.ascii_lowercase + string.digits
     parser = argparse.ArgumentParser()
     parser.add_argument("string", help = "origin flag string")
     args = parser.parse_args()
     entropy = 0
-    for c in args.string:
+    print('hawkHacks{', end='')
+    for c in args.string.replace(' ', '_'):
         new_c, e = change(c)
         print(new_c, end = '')
         entropy += e
-    print()
+    print('_' + ''.join(secrets.choice(chars) for i in range(16)) + '}')
     print(f"Added entropy: {entropy:.2f} bits")
 
 if __name__ == "__main__":
